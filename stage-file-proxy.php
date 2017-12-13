15d6876a170b922a28a363537044f5f9b35edc70
Drew Michael <drew@fruition.net>
2017-12-13 10:39:11 -0700

    added method

--- stage-file-proxy.php
+++ stage-file-proxy.php
@@ -13,16 +13,37 @@
 
 add_action( 'admin_menu', 'stage_file_proxy_menu' );
 function stage_file_proxy_menu() {
-  add_options_page(__('Stage File Proxy Options', 'textdomain'), __('Stage File Proxy Options', 'textdomain'), 'manage_options', 'stage-file-proxy', 'stage_file_proxy_options_page');
+  add_options_page( __('Stage File Proxy Options', 'textdomain' ), __('Stage File Proxy Options', 'textdomain' ), 'manage_options', 'stage-file-proxy', 'stage_file_proxy_options_page' );
 }
 add_action( 'admin_init', 'stage_file_proxy_init' );
 
 function stage_file_proxy_init() {
-  register_setting('stage-file-proxy-group', 'stage-file-proxy-settings', 'stage_file_proxy_settings_validate_and_sanitize');
-  add_settings_section('section-1', __('Source Domain', 'textdomain'), 'section_1_callback', 'stage-file-proxy');
-  add_settings_field('field-1-1', __('Source Domain', 'textdomain'), 'source_domain_callback', 'stage-file-proxy', 'section-1');
-}
 
+  /*
+	 * http://codex.wordpress.org/Function_Reference/register_setting
+	 * register_setting( $option_group, $option_name, $sanitize_callback );
+	 * The second argument ($option_name) is the option name. It’s the one we use with functions like get_option() and update_option()
+	 * */
+  # With input validation:
+  register_setting( 'stage-file-proxy-group', 'stage-file-proxy-settings', 'stage_file_proxy_settings_validate_and_sanitize' );
+
+  /*
+* http://codex.wordpress.org/Function_Reference/add_settings_section
+* add_settings_section( $id, $title, $callback, $page );
+* */
+  add_settings_section( 'section-1', __( 'Source Domain', 'textdomain' ), 'section_1_callback', 'stage-file-proxy' );
+
+  /*
+ * http://codex.wordpress.org/Function_Reference/add_settings_field
+ * add_settings_field( $id, $title, $callback, $page, $section, $args );
+ * */
+  add_settings_field( 'field-1-1', __( 'Source Domain', 'textdomain' ), 'source_domain_callback', 'stage-file-proxy', 'section-1' );
+  add_settings_field( 'field-1-2', __( 'Method', 'select' ), 'method_callback', 'stage-file-proxy', 'section-1' );
+
+}
+/*
+ * THE ACTUAL PAGE
+ * */
 function stage_file_proxy_options_page() {
   ?>
     <div class="wrap">
@@ -34,42 +55,75 @@ function stage_file_proxy_options_page() {
         </form>
     </div>
 <?php }
-
+/*
+* THE SECTIONS
+* Hint: You can omit using add_settings_field() and instead
+* directly put the input fields into the sections.
+* */
 function section_1_callback() {
-  _e('Please enter the domain where the source images are location (for example, http://www.fruition.net).', 'textdomain');
+  _e( 'Please enter the domain where the source images are location (for example, http://www.fruition.net).', 'textdomain' );
 }
-
+/*
+* THE FIELDS
+* */
 function source_domain_callback() {
-  $settings = (array) get_option('stage-file-proxy-settings');
+
+  $settings = (array) get_option( 'stage-file-proxy-settings' );
   $field = "source_domain";
-  $value = esc_attr($settings[$field]);
+  $value = esc_attr( $settings[$field] );
+
   echo "<input type='text' name='stage-file-proxy-settings[$field]' value='$value' />";
 }
 
+function method_callback() {
+
+  $settings = (array) get_option( 'stage-file-proxy-settings' );
+  $field = "method";
+  $value = esc_attr( $settings[$field] );
+
+  echo '<select name="stage-file-proxy-settings[method]">';
+  echo '<option value="download"' . ($value == 'download' ? ' selected="selected"' : '') . '>Download</option>';
+  echo '<option value="redirect"' . ($value == 'redirect' ? ' selected="selected"' : '') . '>Redirect</option>';
+  echo '</select>';
+
+}
+/*
+* INPUT VALIDATION:
+* */
 function stage_file_proxy_settings_validate_and_sanitize( $input ) {
-  $settings = (array) get_option('stage-file-proxy-settings');
-  if (filter_var($input['source_domain'], FILTER_VALIDATE_URL) !== FALSE) {
+
+  $settings = (array) get_option( 'stage-file-proxy-settings' );
+
+  if ( filter_var($input['source_domain'], FILTER_VALIDATE_URL) !== FALSE ) {
     $output['source_domain'] = trim($input['source_domain']);
+  } else {
+    add_settings_error( 'stage-file-proxy-settings', 'invalid-source_domain', 'Please enter a valid domain.' );
   }
-  else {
-    add_settings_error('stage-file-proxy-settings', 'invalid-source_domain', 'Please enter a valid domain.');
-  }
+  $output['method'] = trim($input['method']);
+
   return $output;
 }
 
-function stage_file_proxy_404() {
-  if (is_404() && !isset($_REQUEST['stage_file_proxy'])) {
-    $settings = (array) get_option('stage-file-proxy-settings');
+function stage_file_proxy_404(){
+  if( is_404() && !isset($_REQUEST['stage_file_proxy'])) {
+    $settings = (array) get_option( 'stage-file-proxy-settings' );
     if (isset($settings['source_domain']) && $settings['source_domain'] <> '') {
       if (substr($settings['source_domain'], -1) == '/') {
         $settings['source_domain'] = substr($settings['source_domain'], 0, -1);
       }
       $source = $settings['source_domain'] . $_SERVER['REQUEST_URI'];
+
+      if ($settings['method'] == 'redirect') {
+        header("Location: " . $source);
+        exit;
+      }
+
       $parts = explode('/', $_SERVER['REQUEST_URI']);
       $post_date = "{$parts[3]}-{$parts[4]}-01";
+
       if ($file = file_get_contents($source)) {
         $upload = wp_upload_bits(basename($_SERVER['REQUEST_URI']), NULL, $file, $post_date);
-        if (wp_redirect($upload['url'] . '?stage_file_proxy=true')) {
+        if ( wp_redirect( $upload['url'] . '?stage_file_proxy=true' ) ) {
           exit;
         }
       }
